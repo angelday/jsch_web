9a9381186086970907a036cca79b24340ead885e
József Schaffer <sasvari@gmail.com>
2026-04-11 22:11:29 +0200

    Deploy 2026-04-11 22:11

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { initAboutCursor } from "./about-cursor.js";
+import { initHeroNav } from "./hero-nav.js";
+import { initHeroShader } from "./hero-shader.js";
+import { initTheatreModals } from "./theatre-modal.js";
+
+initHeroShader();
+initHeroNav();
+initAboutCursor();
+initTheatreModals();
